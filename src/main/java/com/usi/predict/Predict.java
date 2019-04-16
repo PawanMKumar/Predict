b22b85faff1efd6e022e4f466f946efe5acf05d4
Medatati <pmedatati@deloitte.com>
2019-04-16 22:38:20 +0530

    first commit to feature branch

--- src/main/java/com/usi/predict/Predict.java
+++ src/main/java/com/usi/predict/Predict.java
@@ -18,7 +18,7 @@ package com.usi.predict;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+ 
 
 @SpringBootApplication
 public class Predict{
